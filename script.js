04e76fb6f3ea65c0d07ac9cddbd7ef33c86996e0
Sergiy Zubenko <radiophaces@gmail.com>
2025-04-10 10:35:47 +0200

    10.04.25 Did some more vids on Udemy

--- script.js
+++ script.js
@@ -8,3 +8,30 @@
 // let population = "48m";
 
 // console.log(country, continent, population);
+
+// const now = 2037;
+// const ageJonas = now - 1988;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2);
+
+// const firstName = "Sergiy";
+// const lastName = "Zubenko";
+// const fullName = firstName + " " + lastName;
+// console.log(fullName);
+// console.log(typeof fullName);
+
+// if (ageJonas > ageSarah) {
+//   console.log("Jonas is older then sarah");
+// }
+
+const userYear = 2020;
+const now = 2025;
+const agePass = now - userYear;
+
+if (agePass >= 18) {
+  console.log("You're age is more then 18");
+} else {
+  console.log("too young");
+}
